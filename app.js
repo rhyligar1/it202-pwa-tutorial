be6423904c888b537dd81f7448f3b20b237598e1
Rasean Hyligar <rhylig2@uic.edu>
2020-04-06 01:43:46 +0000

    No avail with ES6 Arrow Syntax

--- app.js
+++ app.js
@@ -27,27 +27,26 @@ const coffees = [{
     name: "Accusantium",
     image: "images/coffee9.jpg"
 }, ]
-const showCoffees = () => {
-    let output = "";
+const showCoffees = () = > {
+    let output = " ";
     coffees.forEach(({
             name, image
-        }) => (output += <div class = "card">
+        }) => (output += < div class = "card"
             img class = "card--avatar"
             src = $ {
                 image
-            } < h1 class = "card--title" > $ {name}
-               </h1> <a class = "card--link"
-            href = "#" > Taste </a > </div>))
+            } < h1 class = "card--title" > $ {
+                name
+            } </h1> <a class = "card--link"
+            href = "#" > Taste </a > < /div>)
     container.innerHTML = output
 }
 document.addEventListener("DOMContentLoaded", showCoffees)
 
-// Register serviceWorker.js
-if ("serviceWorker" in navigator) {
-  window.addEventListener("load", function() {
-    navigator.serviceWorker
-      .register("/serviceWorker.js")
-      .then(res => console.log("service worker registered"))
-      .catch(err => console.log("service worker not registered", err))
-  })
-}
+/ / Register serviceWorker.js
+            if("serviceWorker" in navigator) {
+                window.addEventListener("load", function() {
+                    navigator.serviceWorker.register("/serviceWorker.js").then(res = > console.log("service worker registered")).
+                    catch(err = > console.log("service worker not registered", err))
+                })
+            }
